1b39e9692b229049a9966be125821351e6420a0c
owlyk <osapyolkina@gmail.com>
2021-11-01 20:12:50 +0200

    MyTask3

--- Lessons/src/main/java/homework3/Puzzle.java
+++ Lessons/src/main/java/homework3/Puzzle.java
@@ -6,23 +6,18 @@ public class Puzzle {
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        System.out.println("Answer the riddle: If I drink, I die. If I eat, I am fine. What am I?");
-        String answer = scan.nextLine();
-    }
-
-    private static void riddleAnswerSwitch(String[] args) {
-        String answer = "Fire";
-        {
-            for (int i = 1; i < 3; i++) {
-                switch (answer) {
-                    case "Fire":
-                        System.out.println("Great!");
-                        break;
-                    case "I don’t know":
-                        System.out.println("Answer: Fire");
-                        break;
-                    default:
-                        System.out.println("Think again");
+        for (int i = 1; i < 4; i++) {
+            System.out.println("Answer the riddle: If I drink, I die. If I eat, I am fine. What am I?");
+            String answer = scan.nextLine();
+            {
+                if (answer.equals("Fire")) {
+                    System.out.println("Great!");
+                    break;
+                } else if (answer.equals("I don’t know")) {
+                    System.out.println("Answer: Fire");
+                    break;
+                } else {
+                    System.out.println("Think again");
                 }
             }
         }
